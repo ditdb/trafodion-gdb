a934120ff3fa09ea8d0b0fc75c05aaf8400b6a18
Guoji.Ma <guoji.ma@gmail.com>
2022-11-09 09:26:03 +0800

    fix bug about regex match

--- OrcaPrinter.py
+++ OrcaPrinter.py
@@ -12,7 +12,7 @@ class Porca(gdb.Command):
         call_str = str(arg) + '->DbgStr()'
         self.val = gdb.parse_and_eval(call_str)
         restr = str(self.val['_M_dataplus']['_M_p'])
-        restr = re.sub('0x.*L', '', restr)
+        restr = re.sub('0x.* L"', '', restr)
         restr = re.sub(r'\\"', '', restr)
         restr = re.sub(r'\\n', '\n', restr)
         print(restr)
