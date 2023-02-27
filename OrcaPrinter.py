51223eff184a6ccc2a8076d8ee2544477db4a331
Guoji.Ma <guoji.ma@gmail.com>
2023-02-27 09:08:01 +0800

    fix bug about regex match of orca

--- OrcaPrinter.py
+++ OrcaPrinter.py
@@ -3,6 +3,9 @@ import sys
 import re
 import gdb
 
+def extendSpace(match):
+    return ' ' * int(match.group(1))
+
 class Porca(gdb.Command):
     """Parse and print tree struct."""
     def __init__ (self):
@@ -14,6 +17,8 @@ class Porca(gdb.Command):
         restr = str(self.val['_M_dataplus']['_M_p'])
         restr = re.sub('0x.* L"', '', restr)
         restr = re.sub(r'\\"', '', restr)
+        restr = re.sub(r'(.*)"$', '\g<1>', restr)
+        restr = re.sub('", . . <repeats ([0-9]*) times>, "', extendSpace, restr)        
         restr = re.sub(r'\\n', '\n', restr)
         print(restr)
 
